debf955fed2a2b5a4df093d6a325a6919434bde7
sowmiyavenkatesan <46124431+sowmiyavenkatesan@users.noreply.github.com>
2020-04-29 17:18:46 +0530

    forwarding credentials to accnt

--- pgRedirect.php
+++ pgRedirect.php
@@ -14,7 +14,8 @@ $CUST_ID = $_POST["CUST_ID"];
 $INDUSTRY_TYPE_ID = $_POST["INDUSTRY_TYPE_ID"];
 $CHANNEL_ID = $_POST["CHANNEL_ID"];
 $TXN_AMOUNT = $_POST["TXN_AMOUNT"];
-
+$EMAIL=$_POST["EMAIL"];
+$MOBILE=$_POST["MOBILE"];
 // Create an array having all required parameters for creating checksum.
 $paramList["MID"] = PAYTM_MERCHANT_MID;
 $paramList["ORDER_ID"] = $ORDER_ID;
@@ -24,9 +25,9 @@ $paramList["CHANNEL_ID"] = $CHANNEL_ID;
 $paramList["TXN_AMOUNT"] = $TXN_AMOUNT;
 $paramList["WEBSITE"] = PAYTM_MERCHANT_WEBSITE;
 
-$paramList["CALLBACK_URL"] = "http://phpzag.com/demo/paytm-payment-gateway-integration-in-php/pgResponse.php";
-$paramList["MSISDN"] = 7777777777; //Mobile number of customer
-$paramList["EMAIL"] = "youremail@gmail.com"; //Email ID of customer
+$paramList["CALLBACK_URL"] = "http://127.0.0.1/paytm/jlpt/pgResponse.php";
+$paramList["MSISDN"] = $MOBILE; //Mobile number of customer
+$paramList["EMAIL"] = $EMAIL; //Email ID of customer
 $paramList["VERIFIED_BY"] = "EMAIL"; //
 $paramList["IS_USER_VERIFIED"] = "YES"; //
 //Here checksum string will return by getChecksumFromArray() function.
