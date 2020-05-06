67bcfad640f1f527c9689cc174d93e730a2ea7b9
sowmiyavenkatesan <46124431+sowmiyavenkatesan@users.noreply.github.com>
2020-05-06 15:25:08 +0530

    Update form_upload.php

--- form_upload.php
+++ form_upload.php
@@ -1,21 +1,44 @@
 <?php
+session_start();
+$fname=$_GET['id'];
+$i = 0; 
+$dir = 'uploads/';
+if ($handle = opendir($dir)) 
+{
+	while (($file = readdir($handle)) !== false)
+	{
+		if (!in_array($file, array('.', '..')) && !is_dir($dir.$file)) 
+			$i++;
+	}
+}	
+$date="2020-10-1";
+if($fname!='')
+{
+if($i<3500 and (date("Y-m-d")<$date))
+{
+$info = pathinfo($_FILES['file']['name']);
+$ext = $info['extension']; // get the extension of the file
+$newname = $fname.".".$ext; 
+$target = 'uploads/'.$newname;
+if (file_exists('uploads/'.$newname))
+{ 
+unlink('uploads/'.$newname);
+move_uploaded_file( $_FILES['file']['tmp_name'], $target);
+echo "uploaded successfully!";
+} 
+else
+{
+move_uploaded_file( $_FILES['file']['tmp_name'], $target);
+echo "uploaded successfully!";
 
- $targetfolder = "uploads/";
-
- $targetfolder = $targetfolder . basename( $_FILES['file']['name']) ;
-
-if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder))
-
- {
-
- echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
-
- }
-
- else {
-
- echo "Problem uploading file";
-
- }
-
- ?>
+}
+}
+else{
+	echo "REGISTRATION CLOSED!!";
+}
+}
+else
+{
+	echo "<center><b>oops something went wrong!</b></center>";
+}
+?>
