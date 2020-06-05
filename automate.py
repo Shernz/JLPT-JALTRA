07e5cdd88d5657cbab0aaef86d5e73ab396e309b
root <adityasreeram99@gmail.com>
2020-06-05 20:52:05 +0530

    ready

--- automate.py
+++ automate.py
@@ -1,22 +1,32 @@
-content=open('form.html')
-filereader=content.read()
+content=open('formfields.txt')
+filereader=content.readlines()
+print(filereader)
 names=[]
-for i in range(len(filereader)-6):
-    x=''
-    if filereader[i]=='n'  and filereader[i+1]=='a' and filereader[i+2]=='m' and filereader[i+3]=='e' and filereader[i+4]=='=':
-        i=i+5
-        while(filereader[i]!=' ' and filereader[i]!='>'):
-            x+=filereader[i]
-            i+=1
-        if x[1:len(x)-1] not in names and len(x)<20:
-            names.append(x[1:len(x)-1])
+for i in filereader:
+    try:
+        print(i.split('.')[1])
+    
+        names.append(i.split('.')[1])
+    except:
+        break
+print(names)
+# names=[]
+# for i in range(len(filereader)-6):
+#     x=''
+#     if filereader[i]=='n'  and filereader[i+1]=='a' and filereader[i+2]=='m' and filereader[i+3]=='e' and filereader[i+4]=='=':
+#         i=i+5
+#         while(filereader[i]!=' ' and filereader[i]!='>'):
+#             x+=filereader[i]
+#             i+=1
+#         if x[1:len(x)-1] not in names and len(x)<20:
+#             names.append(x[1:len(x)-1])
 
-print(len(names))
+# print(len(names))
 sql="create table apply ("
 for i in range(len(names)):
-    sql+=names[i]+" varchar(20),"
+    sql+=names[i]+" varchar(100),"
 print(sql)
-# create table apply (title varchar(20),keywords varchar(20),description varchar(20),viewport varchar(20),news_id varchar(20),user_id varchar(20),save varchar(20),exam_year varchar(20),news_title varchar(20),news_date varchar(20),venue_exam_month varchar(20),payment_amt varchar(20),special_accommoda varchar(20),guide varchar(20),test_level varchar(20),last_2_digit_year varchar(20),name varchar(20),gender varchar(20),dob varchar(20),passcode varchar(20),nativelang varchar(20),street varchar(20),city_state varchar(20),country varchar(20),postalcode varchar(20),telephone_no varchar(20),email varchar(20),institution_name varchar(20),learning_japanese varchar(20),reason_taking varchar(20),occupation varchar(20),media1 varchar(20),media2 varchar(20),media3 varchar(20),media4 varchar(20),media5 varchar(20),media6 varchar(20),media7 varchar(20),media8 varchar(20),media9 varchar(20),teacher1 varchar(20),teacher2 varchar(20),teacher3 varchar(20),teacher4 varchar(20),teacher5 varchar(20),friends1 varchar(20),friends2 varchar(20),friends3 varchar(20),friends4 varchar(20),friends5 varchar(20),family1 varchar(20),family2 varchar(20),family3 varchar(20),family4 varchar(20),family5 varchar(20),supervisor1 varchar(20),supervisor2 varchar(20),supervisor3 varchar(20),supervisor4 varchar(20),supervisor5 varchar(20),colleagues1 varchar(20),colleagues2 varchar(20),colleagues3 varchar(20),colleagues4 varchar(20),colleagues5 varchar(20),customers1 varchar(20),customers2 varchar(20),customers3 varchar(20),customers4 varchar(20),customers5 varchar(20),N1_level1 varchar(20),N2_level2 varchar(20),N3 varchar(20),N4_level4 varchar(20),N5_level5 varchar(20),N1_pass_fail varchar(20),N2_pass_fail varchar(20),N3_pass_fail varchar(20),N4_pass_fail varchar(20),N5_pass_fail varchar(20),photo varchar(20),prephoto varchar(20))
+# # create table apply (title varchar(20),keywords varchar(20),description varchar(20),viewport varchar(20),news_id varchar(20),user_id varchar(20),save varchar(20),exam_year varchar(20),news_title varchar(20),news_date varchar(20),venue_exam_month varchar(20),payment_amt varchar(20),special_accommoda varchar(20),guide varchar(20),test_level varchar(20),last_2_digit_year varchar(20),name varchar(20),gender varchar(20),dob varchar(20),passcode varchar(20),nativelang varchar(20),street varchar(20),city_state varchar(20),country varchar(20),postalcode varchar(20),telephone_no varchar(20),email varchar(20),institution_name varchar(20),learning_japanese varchar(20),reason_taking varchar(20),occupation varchar(20),media1 varchar(20),media2 varchar(20),media3 varchar(20),media4 varchar(20),media5 varchar(20),media6 varchar(20),media7 varchar(20),media8 varchar(20),media9 varchar(20),teacher1 varchar(20),teacher2 varchar(20),teacher3 varchar(20),teacher4 varchar(20),teacher5 varchar(20),friends1 varchar(20),friends2 varchar(20),friends3 varchar(20),friends4 varchar(20),friends5 varchar(20),family1 varchar(20),family2 varchar(20),family3 varchar(20),family4 varchar(20),family5 varchar(20),supervisor1 varchar(20),supervisor2 varchar(20),supervisor3 varchar(20),supervisor4 varchar(20),supervisor5 varchar(20),colleagues1 varchar(20),colleagues2 varchar(20),colleagues3 varchar(20),colleagues4 varchar(20),colleagues5 varchar(20),customers1 varchar(20),customers2 varchar(20),customers3 varchar(20),customers4 varchar(20),customers5 varchar(20),N1_level1 varchar(20),N2_level2 varchar(20),N3 varchar(20),N4_level4 varchar(20),N5_level5 varchar(20),N1_pass_fail varchar(20),N2_pass_fail varchar(20),N3_pass_fail varchar(20),N4_pass_fail varchar(20),N5_pass_fail varchar(20),photo varchar(20),prephoto varchar(20))
 
 d=[]
 variables=[]
